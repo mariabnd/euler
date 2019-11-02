a0d460e0986b547f002235f2b15ea614abf39f30
Maria <33788128+mariabnd@users.noreply.github.com>
2019-11-02 14:31:50 +0100

    Update project6.R
    Wrap the example calculation for project 6 in a function

--- project6.R
+++ project6.R
@@ -1,4 +1,10 @@
-# Example provided
-a <- sum((1 : 10) ** 2) # 385
-b <- sum(1 : 10) ** 2 # 3025
-b - a # 2640
+# Wrap this in a function
+pe_6 <- function(x){
+  a <- sum(x ** 2)
+  b <- sum(x) ** 2
+  return(b - a)
+}
+
+# Example calculations
+pe_6(1 : 10) # 2640
+pe_6(1 : 100) # 25164150
