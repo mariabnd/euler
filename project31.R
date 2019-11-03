6f634d65ed9f5402e337efc07793184ae85e378f
Maria <33788128+mariabnd@users.noreply.github.com>
2019-11-03 20:23:37 +0100

    Update project31.R
    Wrap in a function

--- project31.R
+++ project31.R
@@ -7,30 +7,28 @@
 # 5 = 2 + 1 + 1 + 1
 # 5 = 1 + 1 + 1 + 1 + 1
 
-# So with
-coins <- c(1, 2, 5)
-target <- 5
-# we would like to obtain a value of 4
-
-# Create empty matrix to populate with values
-mixes <- matrix(NA, nrow = length(coins), ncol = target + 1)
-
-# Set target 0 to 1 as there is one way to make up no money
-mixes[1, 1] <- 1
-
-for (i in 1 : length(coins)){
-  if (i > 1) # Cannot add from row above very first row
-    mixes[i, 1 : coins[i]] <- mixes[i - 1, 1 : coins[i]] # Skip lower levels
-  for (j in max(2, coins[i]) : dim(mixes)[2]){
-    ifelse(j - 1 >= coins[i],
-           # If there is a remainder add previous
-           ifelse(i > 1, # Cannot add from row above very first row
-                  mixes[i, j] <- mixes[i, j - coins[i]] + mixes[i - 1, j],
-                  mixes[i, j] <- mixes[i, j - coins[i]]),
-           if (i > 1) # Cannot add from row above very first row
-             mixes[i, j] <- mixes[i - 1, j])
+pe_31 <- function(coins, target){
+  
+  # Create empty matrix to populate with values
+  mixes <- matrix(NA, nrow = length(coins), ncol = target + 1)
+  # Set target 0 to 1 as there is one way to make up no money
+  mixes[1, 1] <- 1
+  
+  for (i in 1 : length(coins)){
+    if (i > 1) # Cannot add from row above very first row
+      mixes[i, 1 : coins[i]] <- mixes[i - 1, 1 : coins[i]] # Skip lower levels
+    for (j in max(2, coins[i]) : dim(mixes)[2]){
+      ifelse(j - 1 >= coins[i],
+             # If there is a remainder add previous
+             ifelse(i > 1, # Cannot add from row above very first row
+                    mixes[i, j] <- mixes[i, j - coins[i]] + mixes[i - 1, j],
+                    mixes[i, j] <- mixes[i, j - coins[i]]),
+             if (i > 1) # Cannot add from row above very first row
+               mixes[i, j] <- mixes[i - 1, j])
+    }
   }
+  # Get the value of interest
+  return(mixes[length(coins), target + 1])
 }
 
-# Get the value of interest
-mixes[length(coins), target + 1] # 4
+pe_31(coins = c(1, 2, 5), target = 5) # 4
